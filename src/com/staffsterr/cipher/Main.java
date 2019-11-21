8639b1746b97707ee270837eaeda907a5420dd91
staffsterr2000 <rosenkostas@gmail.com>
2019-11-21 22:46:26 +0200

    added interface, exceptions and I'm working on encrypt and decrypt methods.

--- src/com/staffsterr/cipher/Main.java
+++ src/com/staffsterr/cipher/Main.java
@@ -16,7 +16,7 @@ public class Main {
         for(int i = 0; i < alphabetChanged.length - (newCells - cells); i++){
             alphabetChanged[i] = alphabet[newCells - cells + i];
         }
-        System.out.println(Arrays.toString(alphabetChanged));
+        System.out.println("Changed alphabet: " + Arrays.toString(alphabetChanged));
     }
 
     public static void alphabetChangeLeft(int cells){
@@ -28,7 +28,52 @@ public class Main {
         for(int i = 0; i < alphabetChanged.length - (newCells - cells); i++){
             alphabetChanged[alphabetChanged.length - cells + i] = alphabet[i];
         }
-        System.out.println(Arrays.toString(alphabetChanged));
+        System.out.println("Changed alphabet: " + Arrays.toString(alphabetChanged));
+    }
+
+    public static String encrypt(String decryptMessageString){
+        String[] decryptMessage = decryptMessageString.split("");
+        String[] encryptMessage = new String[decryptMessage.length];
+
+
+
+
+        return toString(encryptMessage);
+    }
+
+    public static String decrypt(String encryptMessageString){
+        String[] encryptMessage = encryptMessageString.split("");
+        String[] decryptMessage = new String[encryptMessage.length];
+
+
+
+
+        return toString(decryptMessage);
+    }
+
+    public static int findIndex(String[] array, String letter){
+        int index = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i].equals(letter)){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public static String toString(String[] array){
+        String string = "";
+        for(String s: array){
+            string += s;
+        }
+        return string;
+    }
+
+
+    public static void exit() throws IOException{
+        System.out.println("Press ENTER ro exit...");
+        System.in.read();
     }
 
     public static void main(String[] args) throws IOException{
@@ -40,16 +85,45 @@ public class Main {
         String sideChoice = br.readLine();
         boolean isRight = (sideChoice.equalsIgnoreCase("r"));
         boolean isLeft = (sideChoice.equalsIgnoreCase("l"));
-
         System.out.println("Left: " + isLeft);
         System.out.println("Right: " + isRight);
+        if(!(isRight || isLeft)) {
+            exit();
+            return;
+        }
 
-        System.out.print("How many cells you want to skip in this side: ");
-        int cells = (Integer.parseInt(br.readLine()) % 26);
-
+        System.out.print("How many cells you want to skip in this side(>0): ");
+        int cells = Integer.parseInt(br.readLine());
         System.out.println("Cells: " + cells);
+        cells %= 26;
+        if(cells <= 0) {
+            exit();
+            return;
+        }
+
+        System.out.println("Main alphabet: " + Arrays.toString(alphabet));
 
         if(isRight) alphabetChangeRight(cells);
         else if (isLeft) alphabetChangeLeft(cells);
+
+        System.out.print("Do you want to encrypt or decrypt information?(E/D): ");
+        String answer = br.readLine();
+        boolean isEncrypt = answer.equalsIgnoreCase("e");
+        boolean isDecrypt = answer.equalsIgnoreCase("d");
+
+        System.out.println("Encrypt: " + isEncrypt);
+        System.out.println("Decrypt: " + isDecrypt);
+
+        if(isEncrypt){
+            System.out.print("Input info to encrypt: ");
+            String input = br.readLine();
+            System.out.println(encrypt(input));
+        }
+        else if(isDecrypt){
+            System.out.print("Input info to decrypt: ");
+            String input = br.readLine();
+            System.out.println(decrypt(input));
+        }
+        else exit();
     }
 }
